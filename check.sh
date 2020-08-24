0d402b3e10fd9aa8fa3a5fb88954151d4f4910ce
Joran Tesse <jtesse@hubspot.com>
2020-08-24 14:46:47 -0400

    should work

--- check.sh
+++ check.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-git status
+NPM="usb/local/bin/npm"
+GIT="/usr/bin/git"
+
+$GIT fetch origin
+IS_BEHIND=$($GIT status | grep "branch is behind")
+
+if [ "${#IS_BEHIND}" = "0" ]; then
+  exit
+fi
+
+$NPM run stop
+$GIT pull
+$NPM run build
+$NPM start
